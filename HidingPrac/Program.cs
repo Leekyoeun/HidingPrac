56c7c1e54caf9a0a32487b91a4197342d0cb3db5
Leekyoeun <s2018s35@e-mirim.hs.kr>
2020-06-16 10:09:25 +0900

    초기 커밋.

---
+++ HidingPrac/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HidingPrac
+{
+    class Program
+    {
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("Parent!");
+            }
+        }
+        class Child: Parent
+        {
+            public string variable = "shadowing";
+            public void Method()
+            {
+                Console.WriteLine("Child!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            Console.WriteLine("varable:",c.variable);
+            Console.WriteLine("variable:",((Parent)c).variable);
+            c.Method();
+            ((Parent)c).Method();
+        }
+    }
+}
